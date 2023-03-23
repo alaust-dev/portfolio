cb10ca9abbf80f119c5f17603b7e23b250b0605a
Alexander Aust <mail@alexander-aust.de>
2023-03-23 08:51:11 +0100

    Add Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
                 <h2 className="text-[96px] font-amatic text-secondary self-center">Alexander Aust</h2>
                 <h2 className="text-[40px] font-amatic text-white self-end">I&apos;m a Developer</h2>
             </div>
-            <div className="flex flex-col absolute right-[25px] bottom-[25px]">
-                <a className=" flex flex-col content-center">
+            <div className="flex flex-col absolute right-[25px] bottom-[25px] items-center select-none">
+                <a className=" flex flex-col items-center cursor-pointer" target="_blank" href="https://github.com/alaust-dev/">
                     <img src="/github.png" className="w-[64px] h-[64px]"/>
                     <div className="text-center font-abel text-[32px]">GitHub</div>
                 </a>
-                <a className="flex flex-col">
-                    <TbMailFast className="w-[64px] h-[64px]"/>
-                    <div className="text-center font-abel text-[32px]">GitHub</div>
+                <a className="flex flex-col items-center cursor-pointer select-none" href="mailto:mail@alexander-aust.de">
+                    <TbMailFast className="w-[73px] h-[73px] mr-4"/>
+                    <div className="text-center font-abel text-[32px]">Mail Me</div>
                 </a>
             </div>
         </main>
